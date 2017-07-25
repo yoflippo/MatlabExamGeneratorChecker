c7067c026863a1bb19fc3bb84ed8b1553bbe01db
GEBRUIK-PTMCQD3\yoflippo <yoflippo@gmail.com>
2017-07-25 14:11:28 +0200

    Improved the script

---
+++ clean_source/top_mc_function.m
@@ -0,0 +1,12 @@
+%% ==================== BLIJF VAN DE VOLGENDE CODE AF! ====================
+% Alles wat je nodig hebt om deze opdracht af te ronden staat in dit bestand
+% De regels:
+% 1 - Door ons aangemaakte bestandsnamen, commentaar & code mag jij NIET aanpassen
+% 2 - Als het script niet uit te voeren valt, krijg je GEEN punten
+% 3 - Elke variabele waar 'NaN' aan wordt toegekend moet JIJ bewerken.
+%
+% Als je afwijkt van deze regels krijg je geen punten voor deze specifieke
+% weekopdracht
+%
+% Opdrachtbeschrijving
+%
