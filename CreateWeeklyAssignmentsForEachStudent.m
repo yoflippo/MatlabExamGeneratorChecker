30e23036799b0ab2e0bbcec203ae8711cbc46e0c
GEBRUIK-PTMCQD3\yoflippo <yoflippo@gmail.com>
2017-07-26 13:22:18 +0200

    Added new function

--- CreateWeeklyAssignmentsForEachStudent.m
+++ CreateWeeklyAssignmentsForEachStudent.m
@@ -1,30 +1,7 @@
     %% Create the assignments
     clc; close all; clear all;
     
-    nameAssignmentFolder = 'assignments';
-    
-    % Get an overview of files
-    mfiles = readFilesInSubFolder(nameAssignmentFolder,'.m');
-    
-    % Filter the relevant files, only files with 'vraag' are relevant
-    mfilesOI = strfind(mfiles,'vraag_'); %files of interest
-    mfiles = mfiles(cellfun('length',mfilesOI)==2);
-    
-    % Further filter the files down to files without the postfix '_ans'
-    mfilesOI = strfind(mfiles,'_ANT'); %files of interest
-    mfilesOI = mfiles(cellfun('isempty',mfilesOI));
-    
-    % Get HashCode of each file
-    underscorePos = strfind(mfilesOI,'_');
-    numUnderScores = length(underscorePos{2});
-    hashPos = underscorePos{1,2}(numUnderScores-1:numUnderScores);
-
-    for h = 1:length(mfilesOI)
-       HashCodes{h} = mfilesOI{1,h}(hashPos(1)+1:hashPos(2)-1) ;
-    end
-    % Combine the relevant files in a Container (dictionary) so the
-    % hashcode is combined with a location for fast lookup
-    mapWithHash = containers.Map(HashCodes,mfilesOI);
+ 
 
     % Create a working folder called 'student-assignments'
     % Create week folders
---
+++ helpercode/GetDictionaryWithHashAndLocation.m
@@ -0,0 +1,67 @@
+function Dic = GetDictionaryWithHashAndLocation(LocationOfBaseFiles)
+%GETDICTIONARYWITHHASHANDLOCATION This function will get a dictionary with
+%all the HashCodes from the base files generated. This dictionary can be
+%used to quickly compare files.
+%
+% ------------------------------------------------------------------------
+%    Copyright (C) 2017  M. Schrauwen (markschrauwen@gmail.com)
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% ------------------------------------------------------------------------
+%
+% DESCRIPTION:
+%
+%
+% BY: 2017  M. Schrauwen (markschrauwen@gmail.com)
+%
+% PARAMETERS:
+%               varargin:   <text>
+%               varargin:   <text>
+%
+% RETURN:
+%               outvar:     <text>
+%               outvar:     <text>
+%
+% EXAMPLES:
+%
+%
+
+% $Revision: 0.0.0 $  $Date: 20xx-xx-xx $
+%<Description>
+nameAssignmentFolder = LocationOfBaseFiles;
+
+% Get an overview of files
+mfiles = readFilesInSubFolder(nameAssignmentFolder,'.m');
+
+% Filter the relevant files, only files with 'vraag' are relevant
+mfilesOI = strfind(mfiles,'vraag_'); %files of interest
+mfiles = mfiles(cellfun('length',mfilesOI)==2);
+
+% Further filter the files down to files without the postfix '_ans'
+mfilesOI = strfind(mfiles,'_ANT'); %files of interest
+mfilesOI = mfiles(cellfun('isempty',mfilesOI));
+
+% Get HashCode of each file
+underscorePos = strfind(mfilesOI,'_');
+numUnderScores = length(underscorePos{2});
+hashPos = underscorePos{1,2}(numUnderScores-1:numUnderScores);
+
+for h = 1:length(mfilesOI)
+    HashCodes{h} = mfilesOI{1,h}(hashPos(1)+1:hashPos(2)-1) ;
+end
+% Combine the relevant files in a Container (dictionary) so the
+% hashcode is combined with a location for fast lookup
+Dic = containers.Map(HashCodes,mfilesOI);
+
+end
