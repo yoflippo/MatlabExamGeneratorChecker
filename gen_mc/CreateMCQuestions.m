2ea86d6a32744a94c5f37b3cfcd6cf640ce3941e
GEBRUIK-PTMCQD3\yoflippo <yoflippo@gmail.com>
2017-09-07 23:32:33 +0200

    It is working, not perfect, but it is working!

--- gen_mc/CreateMCQuestions.m
+++ gen_mc/CreateMCQuestions.m
@@ -34,109 +34,129 @@ end
 copyfiles(fullfile(pathOfThisFile,weekDir),fullfile(outputDir,weekDir))
 emptyDirRecursiveMFiles(fullfile(outputDir,weekDir))
 
+%% Generate answers (could be made randomnly)
+answerA = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_0.m'));
+answerA = ['% A : ' char(answerA)];
+answerB = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_1.m'));
+answerB = ['% B : ' char(answerB)];
+answerC = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_2.m'));
+answerC = ['% C : ' char(answerC)];
+answerD = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_3.m'));
+answerD = ['% D : ' char(answerD)];
+
 %% Browse the subfolder of weekx
 nVersionMC = 0;
 for nDirs = 1:length(folders)
     numberOfThesesFiles = thesesPathAnswer{nDirs,1}{3};
     currentFilePath =  thesesPathAnswer{nDirs,1}{1};
-    randomFileIndexes = randperm(numberOfThesesFiles);
-    % generate final destination of current question, by inserting the
+    % generate final destination path of current question, by inserting the
     % output folder
     absPathDestination = GetPathOneLevelUp(currentFilePath);
     absPathDestination = replace(absPathDestination,weekDir,fullfile('generated_questions',weekDir));
-    % test for no sequentially (e.g. no '3 4 5 6' but '4 6 8 3'), the
-    % sequential questions could be associated (TRUE / FALSE versions).
-    while contains(num2str(diff(randomFileIndexes)),'1')
-        randomFileIndexes = randperm(numberOfThesesFiles);
-    end
     % test for an even / odd number of files
     blOdd = mod(numberOfThesesFiles,2);
     if blOdd
         nFilesEven = numberOfThesesFiles+1;
     end
-    %% combine to make one question
+    
+    % Generate non-adjacent indices
+    cnt = 1;
+    for ind = 1:numberOfThesesFiles-2
+        for ind2 = ind+2:numberOfThesesFiles
+            randomFileIndexes(cnt) = ind; cnt = cnt + 1;
+            randomFileIndexes(cnt) = ind2; cnt = cnt + 1;
+        end
+    end
+    %     % another way to generate indices BUT OLD
+    %     randomFileIndexes = randperm(numberOfThesesFiles);
+    %     % test for no sequentially (e.g. no '3 4 5 6' but '4 6 8 3'), the
+    %     % sequential questions could be associated (TRUE / FALSE versions).
+    %     while contains(num2str(diff(randomFileIndexes)),'1')
+    %         randomFileIndexes = randperm(numberOfThesesFiles);
+    %     end
+    
+    %% Combine to make one question
     % read header file line and give it the right question number
     headerTxt = ReadLineOfFile(fullfile(pathOfThisFile,'mc_header.m'));
     questionNumber = GetPathOneLevelUp(currentFilePath);
     questionNumber = questionNumber(end);
     headerTxt = replace(headerTxt,'xxx',questionNumber);
-    cnt = 1;
-    finalTxt{cnt,1} = headerTxt; cnt = cnt + 1;
-    finalTxt{cnt,1} = '%'; cnt = cnt + 1; %add empty line
     
-    %% Combine theses
-    % theses1
-    currentIndex = randomFileIndexes(1);
-    ansTheses1 = thesesPathAnswer{nDirs,currentIndex}{2};
-    randomFileIndexes = randomFileIndexes(2:end);
-    theses1 = char(getTextOfFile(thesesPathAnswer{nDirs,currentIndex}{1}));
-    preambleTheses1 = '% Stelling 1:   ';
-    txtTheses1{1} = [preambleTheses1 theses1(1,:)];
-    finalTxt{cnt,1} = txtTheses1{1}; cnt = cnt + 1;
-    
-    % add the right number of tabs to rest of text
-    nLinesOfTheses1 = size(theses1); nLinesOfTheses1 = nLinesOfTheses1(1);
-    if nLinesOfTheses1 > 1
-        for otherLines = 2:nLinesOfTheses1
-            txtTheses1{otherLines} = ['%' sprintf('\t\t\t\t') theses1(otherLines,:)];
-            finalTxt{cnt,1} = txtTheses1{otherLines}; cnt = cnt + 1;
+    %% Generate Questions
+    for nMostIndices = 1:(length(randomFileIndexes)/2)
+        cnt = 1;
+        finalTxt{cnt,1} = headerTxt; cnt = cnt + 1;
+        finalTxt{cnt,1} = '%';       cnt = cnt + 1; %add empty line
+        
+        %% Combine theses
+        % theses1
+        currentIndex = randomFileIndexes(1);
+        randomFileIndexes = randomFileIndexes(2:end);
+        ansTheses1 = thesesPathAnswer{nDirs,currentIndex}{2};
+        theses1 = char(getTextOfFile(thesesPathAnswer{nDirs,currentIndex}{1}));
+        preambleTheses1 = '% Stelling 1:   ';
+        txtTheses1{1} = [preambleTheses1 theses1(1,:)];
+        finalTxt{cnt,1} = txtTheses1{1}; cnt = cnt + 1;
+        
+        % add the right number of tabs to rest of text
+        nLinesOfTheses1 = size(theses1); nLinesOfTheses1 = nLinesOfTheses1(1);
+        if nLinesOfTheses1 > 1
+            for otherLines = 2:nLinesOfTheses1
+                txtTheses1{otherLines} = ['%' sprintf('\t\t\t\t') theses1(otherLines,:)];
+                finalTxt{cnt,1} = txtTheses1{otherLines}; cnt = cnt + 1;
+            end
         end
-    end
-    txtTheses1{nLinesOfTheses1+1} = '%'; %line between theses
-    finalTxt{cnt,1} = txtTheses1{nLinesOfTheses1+1}; cnt = cnt + 1;
-    
-    % theses2
-    currentIndex = randomFileIndexes(1);
-    ansTheses2 = thesesPathAnswer{nDirs,currentIndex}{2};
-    randomFileIndexes = randomFileIndexes(2:end);
-    theses2 = char(getTextOfFile(thesesPathAnswer{nDirs,currentIndex}{1}));
-    preambleTheses2 = '% Stelling 2:   ';
-    txtTheses2{1} = [preambleTheses2 theses2(1,:)];
-    finalTxt{cnt,1} =  txtTheses2{1}; cnt = cnt + 1;
-    % add the right number of tabs to rest of text
-    nLinesOfTheses2 = size(theses2); nLinesOfTheses2 = nLinesOfTheses2(1);
-    if nLinesOfTheses2 > 1
-        for otherLines = 2:nLinesOfTheses2
-            txtTheses2{otherLines} = ['%' sprintf('\t\t\t\t') theses2(otherLines,:)];
-            finalTxt{cnt,1} =  txtTheses2{otherLines}; cnt = cnt + 1;
+        txtTheses1{nLinesOfTheses1+1} = '%'; %line between theses
+        finalTxt{cnt,1} = txtTheses1{nLinesOfTheses1+1}; cnt = cnt + 1;
+        
+        % theses2
+        currentIndex = randomFileIndexes(1);
+        randomFileIndexes = randomFileIndexes(2:end);
+        ansTheses2 = thesesPathAnswer{nDirs,currentIndex}{2};      
+        theses2 = char(getTextOfFile(thesesPathAnswer{nDirs,currentIndex}{1}));
+        preambleTheses2 = '% Stelling 2:   ';
+        txtTheses2{1} = [preambleTheses2 theses2(1,:)];
+        finalTxt{cnt,1} =  txtTheses2{1}; cnt = cnt + 1;
+        % add the right number of tabs to rest of text
+        nLinesOfTheses2 = size(theses2); nLinesOfTheses2 = nLinesOfTheses2(1);
+        if nLinesOfTheses2 > 1
+            for otherLines = 2:nLinesOfTheses2
+                txtTheses2{otherLines} = ['%' sprintf('\t\t\t\t') theses2(otherLines,:)];
+                finalTxt{cnt,1} =  txtTheses2{otherLines}; cnt = cnt + 1;
+            end
         end
+        txtTheses2{nLinesOfTheses2+1} = '%'; %line between theses
+        finalTxt{cnt,1} =  txtTheses2{nLinesOfTheses2+1}; cnt = cnt + 1;
+        
+        %% Add answers
+        finalTxt{cnt,1} = answerA; cnt = cnt + 1;
+        finalTxt{cnt,1} = answerB; cnt = cnt + 1;
+        finalTxt{cnt,1} = answerC; cnt = cnt + 1;
+        finalTxt{cnt,1} = answerD; cnt = cnt + 1;
+        
+        %% Handle the answers parts
+        defaultAnswersTxt = ['A'; 'B'; 'C'; 'D';];
+        currentAnswer = defaultAnswersTxt((ansTheses1 * 2) + ansTheses2 + 1);
+        solAnswerLine = ['Antwoord = ' currentAnswer ';'];
+        % get answer line
+        emptyAnswerLine = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_line.m'));
+        % combine all the lines in a cell
+        finalTxt{cnt,1} =  solAnswerLine;
+        
+        %% Write final files
+        cd(absPathDestination);
+        [a nameQuestion] = GetPathOneLevelUp(absPathDestination);
+        % make question string
+        nVersionMC = nVersionMC + 1;
+        nameQuestion = [nameQuestion '_versie_' num2str(nVersionMC)];
+        nameQuestionSOL = [nameQuestion '_SOL'];
+        % write solution file
+        makeMFileFromCells(nameQuestionSOL,finalTxt);
+        % write empty file
+        finalTxt{cnt,1} = emptyAnswerLine;
+        makeMFileFromCells(nameQuestion,finalTxt);
+        cd(pathOfThisFile)
+        % clear variable to prevent polution of files
+        clear finalTxt
     end
-    txtTheses2{nLinesOfTheses2+1} = '%'; %line between theses
-    finalTxt{cnt,1} =  txtTheses2{nLinesOfTheses2+1}; cnt = cnt + 1;
-    
-    %% Generate answers (could be made randomnly)
-    answerA = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_0.m'));
-    answerA = ['% A : ' char(answerA)];
-    finalTxt{cnt,1} = answerA; cnt = cnt + 1;
-    answerB = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_1.m'));
-    answerB = ['% B : ' char(answerB)];
-    finalTxt{cnt,1} = answerB; cnt = cnt + 1;
-    answerC = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_2.m'));
-    answerC = ['% C : ' char(answerC)];
-    finalTxt{cnt,1} = answerC; cnt = cnt + 1;
-    answerD = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_3.m'));
-    answerD = ['% D : ' char(answerD)];
-    finalTxt{cnt,1} = answerD; cnt = cnt + 1;
-    
-    %% Calculate the right answers
-    defaultAnswersTxt = ['A'; 'B'; 'C'; 'D';];
-    currentAnswer = defaultAnswersTxt((ansTheses1 * 2) + ansTheses2 + 1);
-    solAnswerLine = ['Antwoord = ' currentAnswer ';'];
-    % get answer line
-    emptyAnswerLine = ReadLineOfFile(fullfile(pathOfThisFile,'mc_answer_line.m'));
-    % combine all the lines in a cell
-    finalTxt{cnt,1} =  solAnswerLine;
-    % write final files
-    cd(absPathDestination);
-    [a nameQuestion] = GetPathOneLevelUp(absPathDestination);
-    % make question string
-    nVersionMC = nVersionMC + 1;
-    nameQuestion = [nameQuestion '_versie_' num2str(nVersionMC)];
-    nameQuestionSOL = [nameQuestion '_SOL'];
-    % write solution file
-    makeMFileFromCells(nameQuestionSOL,finalTxt);
-    % write empty file
-    finalTxt{cnt,1} = emptyAnswerLine;
-    makeMFileFromCells(nameQuestion,finalTxt);
-    cd(pathOfThisFile)
 end
