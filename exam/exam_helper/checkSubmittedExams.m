c6ea8bf0d9b1954ed8bcb2aa2645f9107ac754dc
GEBRUIK-PTMCQD3\yoflippo <yoflippo@gmail.com>
2017-12-16 21:01:26 +0100

    Almost finished with checkSubmittedExams script.

--- exam/exam_helper/checkSubmittedExams.m
+++ exam/exam_helper/checkSubmittedExams.m
@@ -2,6 +2,9 @@ function [grades] = checkSubmittedExams(examInfo,ap)
 
 %% Unzip Exams
 zfiles = dir('*.zip');
+if isequal(length(zfiles),0)
+    error([newline mfilename ': ' newline 'No Zip-files found in submitted' newline]);
+end
 for nz = 1:length(zfiles)
     oDirs{nz} = replace(zfiles(nz).name,'.zip','');
     apCurrZip = fullfile(zfiles(nz).folder,zfiles(nz).name);
@@ -12,26 +15,26 @@ end
 %% Check for bonus
 cd(ap.CurrExam)
 if exist('bonus','dir')
-   cd('bonus')
-   matfiles = dirmf('.mat');
-   for n = 1:length(matfiles)
-      load(matfiles(n).name);
-      bonusgrades{n} = studentMatrix;
-   end 
-   
-   % Calculate bonusgrade to add to examgrade
-   for len = 1:length(bonusgrades{1,1})
-       bonus{len,1} = bonusgrades{1,1}(len,1);
-       bonusExam{len,1} = bonus{len,1};
-       for wk = 1:length(bonusgrades)         
+    cd('bonus')
+    matfiles = dirmf('.mat');
+    for n = 1:length(matfiles)
+        load(matfiles(n).name);
+        bonusgrades{n} = studentMatrix;
+    end
+    
+    % Calculate bonusgrade to add to examgrade
+    for len = 1:length(bonusgrades{1,1})
+        bonus{len,1} = bonusgrades{1,1}(len,1);
+        bonusExam{len,1} = bonus{len,1};
+        for wk = 1:length(bonusgrades)
             bonus{len,wk+1} = bonusgrades{1,wk}(len,2);
             bonusExam{len,wk+1} = (bonus{len,wk+1}-1)/(length(bonusgrades)*9);
-       end
-       bonusExam{len,length(bonusgrades)+2} = sum([bonusExam{len,2:5}]);
-   end
-   clear studentMatrix bonusgrades n len matfiles
+        end
+        bonusExam{len,length(bonusgrades)+2} = sum([bonusExam{len,2:5}]);
+    end
+    clear studentMatrix bonusgrades n len matfiles
 end
-
+cd(ap.Submitted)
 
 %% Walk through unzipped folders
 for nd = 1:length(oDirs)
@@ -47,7 +50,23 @@ for nd = 1:length(oDirs)
     grade = gradeExamFiles(examInfo);
     
     %% Create file with grade information
-    cCheck_GradeText;
+    t{1} = ['% Jouw tentamen cijfer: ' num2str(round(grade,1))];
+    studBonusGrade = 0;
+    if exist('bonus','var')
+        idx = find([bonus{:,1}]==currStudentNumber);
+        for wk = 1:4
+            if bonus{idx,wk+1} > 1
+                t{length(t)+1} = ['% Bonus cijfer week ' num2str(wk) ': ' num2str(bonus{idx,wk+1}) ' (' num2str(round(bonusExam{idx,wk+1},2)) ')'];
+            end
+        end
+        [l w] = size(bonusExam);
+        studBonusGrade = bonusExam{idx,w};
+    end
+    studFinGrade = round(grade+studBonusGrade,1);
+    t{length(t)+1} = ['%% Jouw eindcijfer: ' num2str(studFinGrade)];
+    t{length(t)+1} = '% Kijk eventueel in het UITWERKING bestand om te zien wat er fout is gegaan.';
+    t{length(t)+1} = '% Alleen bij het Tentamen (niet het HERtentamen) wordt';
+    t{length(t)+1} = '% het bonuspunt opgeteld.';
     makeMFileFromCells(fullfile(pwd,'JouwCijfer'),t);
     
     %% Zip checked exam
@@ -55,8 +74,8 @@ for nd = 1:length(oDirs)
     zip(['Checked_' oDirs{nd} '.zip'],oDirs{nd})
     
     %% Save grade
-    grades(nd,1:2) = [currStudentNumber grade];
-    
+    grades(nd,1:2) = [currStudentNumber studFinGrade];
+    clear idx studBonusGrade studFinGrade t
 end
 
 save('grades.mat','grades');
