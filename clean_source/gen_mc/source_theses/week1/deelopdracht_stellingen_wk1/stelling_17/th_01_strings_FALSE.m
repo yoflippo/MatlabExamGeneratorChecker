cfc0221e81af61665791fa9df5318496fee63151
DESKTOP-HT1P5ES\yoflippo <yoflippo>
2018-12-05 14:06:21 +0100

    Analyse Bonus Assignments is more or less finished with some nice Matlab Window Environment action.

--- clean_source/gen_mc/source_theses/week1/deelopdracht_stellingen_wk1/stelling_17/th_01_strings_FALSE.m
+++ clean_source/gen_mc/source_theses/week1/deelopdracht_stellingen_wk1/stelling_17/th_01_strings_FALSE.m
@@ -1,2 +1,9 @@
-In Matlab wordt een stuk tekst zoals
-'ditiseenstuktekst' ook wel een string genoemd.
+Zie onderstaande code voorbeeld. 
+In Matlab wordt de tekst in de variabele var een string genoemd. 
+Tip: merk het verschil tussen een character vector en een string op.
+
+======= Code =======
+
+var = 'ditiseenstuktekst';
+
+======= Code =======
--- helpercode/AnalyseBonusAssignments.m
+++ helpercode/AnalyseBonusAssignments.m
@@ -22,14 +22,15 @@ dcasap = dicNameAssignmentAndPoints;
 
 %% Traverse students
 strStudGrad = [];
-nT = 1;
-for nSt = 2:length(asas(2:end,1))
-    currAssStud = asas(nSt,2:end);
+for nSt = 1:length(asas(2:end,1))
+    currAssStud = asas(nSt+1,2:end);
     currAssStudNotEmpty = sum(~cellfun(@isempty,currAssStud));
     if currAssStudNotEmpty>0
-        strStudGrad(nT).idx = nSt;
-        strStudGrad(nT).sumPoints = sum(cell2mat(currAssStud));
-        nT = nT + 1;
+        strStudGrad(nSt).idx = nSt;
+        strStudGrad(nSt).sumPoints = sum(cell2mat(currAssStud));
+    else
+        strStudGrad(nSt).idx = nSt;
+        strStudGrad(nSt).sumPoints = [];
     end
 end
 
@@ -39,14 +40,15 @@ for nAss = 2:length(asas(1,2:end))
     % Get info out of dictionaries
     currHash = asas{1,nAss};
     strOV(nAss-1).Hash = currHash;
-    strOV(nAss-1).apAss = dchap(currHash);
+    strOV(nAss-1).apAssSol = dchap(currHash);
+    strOV(nAss-1).apAss = replace(dchap(currHash),'_SOL','');
     rpCurrAss = extractAfter(dchap(currHash),nmCurrBonusAss);
     rpCurrAss = fileparts(rpCurrAss); %remove file
     rpCurrAss = rpCurrAss(2:end); %remove first slash
     strOV(nAss-1).rpAss = rpCurrAss;
     pntCurrAss = dcasap(rpCurrAss);
     strOV(nAss-1).pntAss = pntCurrAss;
-% %     min(min(corrcoef(desstat.resPoint(:,nl),desstat.totPoints)));
+    
     % Get clean points
     currPoints = asas(2:end,nAss);
     assEmpty = cellfun(@isempty,currPoints);
@@ -59,6 +61,66 @@ for nAss = 2:length(asas(1,2:end))
     % Calculate P-value current assignment
     strOV(nAss-1).meanPoints = mean(cell2mat(clnPoints)*strOV(nAss-1).pntAss);
     strOV(nAss-1).Pval = strOV(nAss-1).meanPoints/strOV(nAss-1).pntAss;
+    % Calculate RIT-value
+    strOV(nAss-1).RIT = min(min(corrcoef(cell2mat(clnPoints), ...
+        cell2mat({strStudGrad(assNotEmpty).sumPoints}'))));
+end
+
+%% Give back the problem cases
+minValP = 0.2;
+minValRit = 0.2;
+
+problem.PValues = [strOV.Pval]<minValP;
+problem.RITValues = [strOV.RIT]<minValRit;
+problem.Big = problem.PValues & problem.RITValues;
+problem.All = problem.PValues | problem.RITValues;
+
+problem.AssLowP.SOL = {strOV(problem.PValues).apAssSol};
+problem.AssLowP.Ass = {strOV(problem.PValues).apAss};
+
+problem.AssLowRIT.SOL = {strOV(problem.RITValues).apAssSol};
+problem.AssLowRIT.Ass = {strOV(problem.RITValues).apAss};
+
+problem.AssBigProb.SOL = {strOV(problem.Big).apAssSol};
+problem.AssBigProb.Ass = {strOV(problem.Big).apAss};
+
+problem.AssAll.SOL = {strOV(problem.All).apAssSol};
+problem.AssAll.Ass = {strOV(problem.All).apAss};
+
+%% Ask user to go through all problem files
+if isequal(input('Do you want to go through all problem files? Yes = 1, No = else '),1)
+    for nF = 1:length(problem.All)
+        clc;
+        disp(['There are ' num2str(sum(problem.All)) ' problems found'])
+        currSOLFile = problem.AssAll.SOL{nF};
+        currAssFile = problem.AssAll.Ass{nF};
+        if any(contains(problem.AssLowP.SOL,currSOLFile))
+            disp('This file has a problem with the P-value');
+        elseif any(contains(problem.AssLowRIT.SOL,currSOLFile))
+            disp('This file has a problem with the RIT-value');
+        end
+        disp('Remember that these files are copies and you should also make changes in cleansource');
+        edit(currSOLFile);
+        edit(currAssFile);
+        % Get some editor information for setting the carret and closing
+        % files
+        edtSvc  = com.mathworks.mlservices.MLEditorServices ;
+        edtList = edtSvc.getEditorApplication.getOpenEditors.toArray ;
+        edtList(end).bringToFront;
+        endPosition = edtList(end).getDocument.getLength;
+        edtList(end).setCaretPosition(endPosition);
+        % Also set caret for second to last files
+        endPosition = edtList(end-1).getDocument.getLength;
+        edtList(end-1).setCaretPosition(endPosition);
+        commandwindow
+        if isequal(input('Do you want to close The opened scripts? Yes = 1, No = else '),1)
+            % Close opened scripts
+            edtSvc  = com.mathworks.mlservices.MLEditorServices ;
+            edtList = edtSvc.getEditorApplication.getOpenEditors.toArray ;
+            edtList(end).close;
+            edtList(end-1).close;
+        end
+    end
 end
 
 end % function
